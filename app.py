4623a9c6101611d2f3a359e9340ae1a83ecbe3b5
SARBAJITK805 <134982906+SARBAJITK805@users.noreply.github.com>
2024-07-29 17:28:36 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template,request
+
+import pickle
+
+cv=pickle.load(open("models/cv.pkl","rb"))
+clf=pickle.load(open("models/clf.pkl","rb"))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    if request.method=="POST":
+        email=request.form.get("email-content")
+    tokenized_email=cv.transform([email])
+    predictions=clf.predict(tokenized_email)
+
+    predictions=1 if predictions==1 else -1
+
+    return render_template("index.html",predictions=predictions,email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
