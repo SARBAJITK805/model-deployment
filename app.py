399117d047fdf7ec39513d66606233af5e01937d
SARBAJITK805 <134982906+SARBAJITK805@users.noreply.github.com>
2024-07-29 17:59:26 +0000

    API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,jsonify
 
 import pickle
 
@@ -21,6 +21,16 @@ def predict():
     predictions=1 if predictions==1 else -1
 
     return render_template("index.html",predictions=predictions,email=email)
+
+@app.route("/api/predict",methods=["POST"])
+def api_predict():
+    data=request.get_json(force=True)
+    email=data["content"]
+    tokenized_email=cv.transform([email])
+    predictions=clf.predict(tokenized_email)
+
+    predictions=1 if predictions==1 else -1
+    return jsonify({prediction:prediction})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
